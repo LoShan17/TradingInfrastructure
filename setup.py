bb4e77cc0ddf0889abac0ac3ac28dd7094ecfc8a
Daniel Shamoa <shamoa.daniel@gmail.com>
2016-03-09 12:17:27 +0000

    First commit for TradingInfrastructure package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+#['strategies_backtester']
+
+setup(name = 'TradingInfrastructure',
+      version = '0.1.1',
+      description = 'Package that creates a Trading Infrastructure framework',
+      author = 'Daniel Shamoa',
+      packages = find_packages(),
+      zip_safe = False)
